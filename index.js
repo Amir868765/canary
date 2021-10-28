5128d5d6e4e2b123211882f295370b38dd8fd93a
Mosbah Amir <mosbaha50gmosbahmosbaha50@gmail.com>
2021-10-28 11:29:17 +0100

    addfichier

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+  });
+  app.get('/signin', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/signin.html'));
+  });
+  app.get('/signup', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/signup.html'));
+  });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
